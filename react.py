628762272e018cceae1d7629597852dccf92f11d
AndriiSyniuchenko <andriisyniuchenko@gmail.com>
2026-05-03 19:16:05 -0700

    graph

--- react.py
+++ react.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from langchain_core.tools import tool
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
 
 load_dotenv()
@@ -17,4 +17,4 @@ def triple(num:float) -> float:
 tools = [TavilySearch(max_result=1), triple]
 
 
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
+llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0).bind_tools(tools)
